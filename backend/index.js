0205633f2e7c95c1868e3bfa88ee845234d4743f
bar <barkatz0610@gmail.com>
2021-09-25 12:48:18 +0300

    UPDATES

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,6 @@ const nodemailer = require("nodemailer");
 const app = express();
 app.use(cors({ origin: "*" }));
 app.use(bodypraser.json());
-
 var dateTime=new Date();
 
 const storage=multer.diskStorage({
@@ -19,13 +18,9 @@ const storage=multer.diskStorage({
         callBack(null,`${dateTime.toISOString().slice(0,10)}_${file.originalname}`)
     }
 });
-
 var upload= multer({storage: storage});
-//post-upload and send email 
 app.post('/file/:email',upload.single('file'),(req,res,next)=>{
     const file=req.file;
-   // console.log(file.filename);
-    //console.log(file.path);
     const email=req.params.email;
     if(!file){
         const error= new Error('Please upload file')
@@ -33,10 +28,8 @@ app.post('/file/:email',upload.single('file'),(req,res,next)=>{
         return next(error)
     }
     sendMail(email,file.path,file.filename);
-  
-    res.send(file)
+    res.send(file);
 });
-
 function sendMail(email,filePath,fileName) {
     var transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
@@ -47,31 +40,22 @@ function sendMail(email,filePath,fileName) {
           pass: 'pvdy buyh kjbg vnsy', // generated ethereal password
         },
       });
-
       var mailOption={
           from: 'weTransferCo@gmail.com',
           to: email,
           subject: 'We Tranfser FILE',
-          //text: `Hello, We send you your file that you upload. Now you can download it also here! `,
           html: '<h1>Hello</h1> <p> We send you your file that you uploaded. </p> <p>Now you can also download it here! </p>',
           attachments: {
               filename: fileName,
               path: filePath,
           }
-          
       }
       transporter.sendMail(mailOption,function(err, info){
             if(err) console.log("Something wrong =>",err);
-
             else console.logl('Email sent:' + info.messageId);//this doesnt work
       });
-
 }
-
-
-//download the file
 app.get('/file/:path',(req,res,next)=>{
-
     const filePath= `myFiles/${req.params.path}` ;
     console.log('file path',filePath)
     res.download(filePath,(err)=>{
@@ -81,7 +65,6 @@ app.get('/file/:path',(req,res,next)=>{
         }
     });
 });
-
 app.listen(3000, () => {
     console.log("The server started on port 3000 !!!!!!");
 });
--- frontend/src/app/components/user-file/user-file.component.ts
+++ frontend/src/app/components/user-file/user-file.component.ts
@@ -18,69 +18,47 @@ export class UserFileComponent implements OnInit {
   file:File | undefined;
   multipleFilesNames : string[]=[];
 
-  
-
   constructor(private transferService : TransferService) { }
 
-  ngOnInit(): void {
- 
-  }
+  ngOnInit(): void {  }
 
   validateEmail(email:string) {
- const regularExpression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
- return regularExpression.test(String(email).toLowerCase());
-}
-
-  onChange(event:any){
-      this.file=event.target.files[0];
+    const regularExpression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regularExpression.test(String(email).toLowerCase());
   }
-
+  onChange(event:any){
+    this.file=event.target.files[0];
+  }
   onUpload():void{
-    console.log("our form=>",this.form);
-   let email=this.form.controls.name.controls.email.value;
+    let email=this.form.controls.name.controls.email.value;
     if(this.file &&  this.validateEmail(email)){
       this.loading=!this.loading;
       this.transferService.upload(this.file,email).subscribe(
       (event:any)=>{
-        if (typeof (event) === 'object') {
-          // push the file name to the array 
+        if (typeof (event) === 'object') { 
           this.multipleFilesNames.push(event.filename);
-     //     console.log(event)
-          this.loading = false; // Flag variable 
+          this.loading = false;
           this.onDownload(event.filename);
        }
       }
       );
     }
-
   }
-
-  
- // onDownload(path : string):void{
-   // this.transferService.getFile(path).subscribe(data=>{//data will be Blob like this: Blob {size: 4914, type: “image/png”}
-    //console.log('data=>',data);
-     // let downloadURL=window.URL.createObjectURL(data);//convert the blob into url
-   //  console.log('download url=>',downloadURL.blink())
-      //saveAs(downloadURL);//to download
-    //})
-  //}
 
   onDownload(path:string):void{
     this.transferService.getFile(path).subscribe(data=>{
-          let downloadLink = document.createElement('a');
-          console.log('data=>',data);
-            downloadLink.href = window.URL.createObjectURL(data);
-            if (path){
-                downloadLink.setAttribute('download', path);
-                downloadLink.setAttribute('class','link');
-              
-            document.body.appendChild(downloadLink);
-            console.log('downloadLink',downloadLink);
-            const newText=document.createTextNode("Here");
-            downloadLink.appendChild(newText);
-            const currentDiv = document.getElementById("td");
-            document.body.insertBefore(downloadLink, currentDiv);
-          }
+      let downloadLink = document.createElement('a');
+      console.log('data=>',data);
+      downloadLink.href = window.URL.createObjectURL(data);
+      if (path){
+        downloadLink.setAttribute('download', path);
+        downloadLink.setAttribute('class','link');
+        document.body.appendChild(downloadLink);
+        const newText=document.createTextNode("Here");
+        downloadLink.appendChild(newText);
+        const currentDiv = document.getElementById("td");
+        document.body.insertBefore(downloadLink, currentDiv);
+      }
     });
   }
 
